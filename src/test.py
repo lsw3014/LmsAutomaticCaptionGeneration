20dc5cdbb9aba3765ff03771ebceaa7bb0c7275d
lsw3014 <106336933+lsw3014@users.noreply.github.com>
2024-10-10 19:38:44 +0900

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,16 +1,18 @@
+# pyaudio test
 import pyaudio
 
-# Create an instance of PyAudio
+# pyaudio 객체 생성
 p = pyaudio.PyAudio()
 
 def select_microphone(index):
-   # Get the device info
+   # 기기 정보 불러오기
    device_info = p.get_device_info_by_index(index)
-   # Check if this device is a microphone (an input device)
+   
+   # 입력 장치인지 확인
    if device_info.get('maxInputChannels') > 0:
       print(f"Selected Microphone: {device_info.get('name')}")
    else:
       print(f"No microphone at index {index}")
 
-# Select a microphone with a specific index
+# 인덱스로 특정 입력 장치 선택
 select_microphone(1)
